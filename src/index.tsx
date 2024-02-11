99f798e2876945e12b0921b6d969ee0631bee193
Nazar Maksymchuk <nzrbeats@gmail.com>
2024-02-11 01:27:08 -0800

    out of order prototype

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,13 @@
 import { Hono } from 'hono'
 import { renderer } from './renderer'
 import { Suspense } from 'hono/jsx'
-import { renderToReadableStream } from 'hono/jsx/streaming'
 
-
-async function AsyncComponent() {
+async function AsyncComponentTwo() {
   await new Promise((resolve) => setTimeout(resolve, 2000))
+  return <h1>Fake async hahaha</h1>
+}
+async function AsyncComponent() {
+  await new Promise((resolve) => setTimeout(resolve, 4000))
   return <h1>Fake async hahaha</h1>
 }
 
@@ -16,19 +18,26 @@ app.get('*', renderer)
 
 app.get('/', (c) => {
   return c.render(
-    <Suspense fallback={<h1>Hello!</h1>}>
-      <AsyncComponent />
-    </Suspense>
+    <>
+      <Suspense fallback={<h1>Out!</h1>}>
+        <AsyncComponentTwo />
+      </Suspense>
+      <Suspense fallback={<h1>Of!</h1>}>
+        <AsyncComponent />
+      </Suspense>
+      <Suspense fallback={<h1>Order!</h1>}>
+        <AsyncComponentTwo />
+      </Suspense>
+    </>
   )
 })
 
 app.get('/stream', async (c) => {
-  const children = (
+  const body = await c.render(
     <Suspense fallback={<h1>Partial Streaming</h1>}>
       <AsyncComponent />
     </Suspense>
-  )
-  const body = await c.render(children).body
+  ).body
   return c.body(body, {
     headers: {
       'Content-Type': 'text/html; charset=UTF-8',
