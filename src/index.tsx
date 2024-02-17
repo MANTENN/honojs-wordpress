954bc46f49f01593e37e5aed076eebdc019dfe67
Nazar Maksymchuk <nzrbeats@gmail.com>
2024-02-16 20:37:43 -0800

    source wp data from planetscale

--- src/index.tsx
+++ src/index.tsx
@@ -2,40 +2,76 @@ import { Hono } from 'hono'
 import { renderer } from './renderer'
 import { Suspense } from 'hono/jsx'
 
-async function AsyncComponentTwo({ content = "Oops" }) {
+import { sql } from 'drizzle-orm'
+import { PlanetScaleDatabase, drizzle } from "drizzle-orm/planetscale-serverless";
+import { connect } from "@planetscale/database";
+
+type Bindings = {
+  DATABASE_HOST: string
+  DATABASE_USERNAME: string
+  DATABASE_PASSWORD: string
+}
+
+
+async function AsyncComponentTwo({ content = "Oops", db, first = 1, offset }: { content: string, db: PlanetScaleDatabase<Record<string, never>>, first?: number, offset: number }) {
   await new Promise((resolve) => setTimeout(resolve, 2000))
-  return <h1>{content}</h1>
+  const posts: any[] = (await db.execute(sql`SELECT * FROM wp_posts limit ${first} OFFSET ${offset};`)).rows;
+  return (<>
+    {posts.map((post) => <h1>{post.post_title}</h1>)}
+  </>)
 }
-async function AsyncComponent({ content = "Oops2" }) {
+async function AsyncComponent({ content = "Oops2", db, first = 1, offset }: { content: string, db: PlanetScaleDatabase<Record<string, never>>, first?: number, offset: number }) {
   await new Promise((resolve) => setTimeout(resolve, 4000))
-  return <h1>{content}</h1>
+  const posts: any[] = (await db.execute(sql`SELECT * FROM wp_posts limit ${first} OFFSET ${offset};`)).rows;
+  return (<>
+    {posts.map((post) => <h1>{post.post_title}</h1>)}
+  </>)
 }
 
 
-const app = new Hono()
+const app = new Hono<{ Bindings: Bindings }>()
 
 app.get('*', renderer)
 
-app.get('/', (c) => {
+app.get('/', async (c) => {
+  const mysqlConnection = connect({
+    host: c.env.DATABASE_HOST,
+    username: c.env.DATABASE_USERNAME,
+    password: c.env.DATABASE_PASSWORD,
+  })
+
+  const db = drizzle(mysqlConnection);
+
   return c.render(
     <>
       <Suspense fallback={<h1>1</h1>}>
-        <AsyncComponentTwo content={"Out"} />
+        <AsyncComponentTwo content={"Out"} db={db} first={1} offset={0} />
       </Suspense>
-      <Suspense fallback={<h1>2</h1>}>
-        <AsyncComponent content={"Of"} />
+      <Suspense fallback={<h1>2</h1>} >
+        <AsyncComponent content={"Of"} db={db} offset={2} />
       </Suspense>
       <Suspense fallback={<h1>3</h1>}>
-        <AsyncComponentTwo content={"Order"} />
+        <AsyncComponentTwo content={"Order"} db={db} offset={3} />
       </Suspense>
-    </>
+    </>,
+    {
+      title: "out of order home"
+    }
   )
 })
 
 app.get('/stream', (c) => {
+  const mysqlConnection = connect({
+    host: c.env.DATABASE_HOST,
+    username: c.env.DATABASE_USERNAME,
+    password: c.env.DATABASE_PASSWORD,
+  })
+
+  const db = drizzle(mysqlConnection);
+
   return c.render(
     <Suspense fallback={<h1>Partial Streaming</h1>}>
-      <AsyncComponent />
+      <AsyncComponent content={"Order"} db={db} offset={3} />
     </Suspense>
   )
 })
--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,5 @@
+import { getEnv } from '@hono/vite-dev-server/cloudflare-pages'
+
 import build from '@hono/vite-cloudflare-pages'
 import devServer from '@hono/vite-dev-server'
 import { defineConfig } from 'vite'
@@ -6,6 +8,13 @@ export default defineConfig({
   plugins: [
     build(),
     devServer({
+      env: getEnv({
+        bindings: {
+          DATABASE_HOST: 'aws.connect.psdb.cloud',
+          DATABASE_USERNAME: 'chplmuv4upf2diop20te',
+          DATABASE_PASSWORD: 'pscale_pw_FC3SVKn7H4ivjYrdHBftJcpLLUaqKpU46keMrxwNWuc',
+        },
+      }),
       entry: 'src/index.tsx'
     })
   ]
